21dfbf5fa223870789ebded7e7a2547bbc0b1727
Axel Fontana <axelfontana@gmail.com>
2023-04-01 19:05:19 -0300

    entity models created

---
+++ app/Models/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name', 'size', 'image', 'price', 'brand_id', 'category_id'
+    ];
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
