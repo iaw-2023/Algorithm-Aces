cadc4fe955fcbd769658bef46ad08a05b31e09fd
Axel Fontana <axelfontana@gmail.com>
2023-04-16 18:42:39 -0300

    Api routes for controllers updated

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\OrderDetailController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ShoppingCartController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,18 +28,42 @@ Route::prefix('api')->group(function () {
     Route::prefix('brands')->group(function () {
         Route::get('/', [BrandController::class, 'index']);
         Route::get('/{id}', [BrandController::class, 'show']);
-        Route::post('/', [BrandController::class, 'store']);
-        Route::put('/{brand}', [BrandController::class, 'update']);
-        Route::delete('/{brand}', [BrandController::class, 'delete']);
     });
 
     Route::prefix('categories')->group(function () {
         Route::get('/', [CategoryController::class, 'index']);
         Route::get('/{id}', [CategoryController::class, 'show']);
-        Route::post('/', [CategoryController::class, 'store']);
-        Route::put('/{category}', [CategoryController::class, 'update']);
-        Route::delete('/{category}', [CategoryController::class, 'delete']);
     });
+
+    Route::prefix('products')->group(function () {
+        Route::get('/', [ProductController::class, 'index']);
+        Route::get('/{id}', [ProductController::class, 'show']);
+    });
+
+    Route::prefix('order-detail')->group(function () {
+        Route::get('/', [OrderDetailController::class, 'index']);
+        Route::get('/{id}', [OrderDetailController::class, 'show']);
+        Route::post('/', [OrderDetailController::class, 'store']);
+        Route::put('/{id}', [OrderDetailController::class, 'update']);
+        Route::delete('/{id}', [OrderDetailController::class, 'destroy']);
+    });
+
+    Route::prefix('shopping-cart')->group(function () {
+        Route::get('/', [ShoppingCartController::class, 'index'])->name('index');
+        Route::get('/{id}', [ShoppingCartController::class, 'show'])->name('show');
+        Route::post('/', [ShoppingCartController::class, 'store'])->name('store');
+        Route::put('/{id}', [ShoppingCartController::class, 'update'])->name('update');
+        Route::delete('/{id}', [ShoppingCartController::class, 'destroy'])->name('destroy');
+    });
+
+    Route::prefix('clients')->group(function ()  {
+        Route::get('/', [ClientController::class, 'index']);
+        Route::get('/{id}', [ClientController::class, 'show']);
+        Route::post('/', [ClientController::class, 'store']);
+        Route::put('/{id}', [ClientController::class, 'update']);
+        Route::delete('/{id}', [ClientController::class, 'destroy']);
+    });
+
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
