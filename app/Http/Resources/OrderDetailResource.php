6d8e596fd30874cdfb6682ff31cf81d068a874c9
Axel Fontana <axelfontana@gmail.com>
2023-04-16 18:32:33 -0300

    ClientResource, OrderDetailResource,ProductResource,ShoppingCartResource created

---
+++ app/Http/Resources/OrderDetailResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderDetailResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'product_amount' => $this->product_amount,
+            'product' => new ProductResource($this->product),
+            'shopping_cart' => new ShoppingCartResource($this->shoppingCart),
+            'created_at' => $this->created_at->toDateTimeString(),
+            'updated_at' => $this->updated_at->toDateTimeString(),
+        ];
+    }
+}
---
+++ app/Http/Resources/ShoppingCartResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ShoppingCartResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'total_price' => $this->total_price,
+            'date' => $this->date,
+            'client' => new ClientResource($this->client),
+            'orders_detail' => OrderDetailResource::collection($this->ordersDetail),
+            'created_at' => $this->created_at->toDateTimeString(),
+            'updated_at' => $this->updated_at->toDateTimeString(),
+        ];
+    }
+}
