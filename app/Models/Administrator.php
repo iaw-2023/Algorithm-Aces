7cf9eac0bdb569e7f94c49c02b1a7b91495265e5
Axel Fontana <axelfontana@gmail.com>
2023-04-22 16:59:17 -0300

    Se cambia el atributo 'user' por 'email' en la  migration y modelo de admin

--- app/Models/Administrator.php
+++ app/Models/Administrator.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Administrator extends Authenticatable
 {
-    protected $fillable = ['user','password'];
+    protected $fillable = ['email','password'];
 
-    
+
 }
--- database/migrations/2023_03_27_214013_create_administrators_table.php
+++ database/migrations/2023_03_27_214013_create_administrators_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('administrators', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('user')->unique();
+            $table->string('email')->unique();
             $table->string('password')->nullable();
             $table->timestamps();
         });
