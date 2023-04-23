b018dd8c9258ed20c359cc822069c876947fd8ba
Axel Fontana <axelfontana@gmail.com>
2023-04-22 22:00:24 -0300

    Se agrega un rememberToken al migration de admnistrator, para poder mantenerse logueado. Se agrega el import correspondiente en ResetPasswordController

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class ResetPasswordController extends Controller
@@ -19,7 +20,7 @@ class ResetPasswordController extends Controller
     |
     */
 
-    use ResetsPasswords;
+    use ResetsPasswords,CanResetPassword;
 
     /**
      * Where to redirect users after resetting their password.
--- app/Models/Administrator.php
+++ app/Models/Administrator.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class Administrator extends Authenticatable
 {
+    use Notifiable,HasFactory,CanResetPassword;
     protected $fillable = ['email','password'];
 
 
