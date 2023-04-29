6692996bbeec97a60460eb38902d31f4b7a60224
Axel Fontana <axelfontana09@gmail.com>
2023-04-29 11:07:55 -0300

    CRUD BrandController added

---
+++ app/Http/Controllers/ViewControllers/BrandController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\ViewControllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use Illuminate\Http\Request;
+
+class BrandController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $brands = Brand::all();
+        return view ('BrandViews.brands',compact('brands'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('BrandViews.brand-create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validateData = $request->validate ([
+            'name' => 'required|max:255',
+        ]);
+        Brand::create([
+            'name' => $validateData['name'],
+        ]);
+
+        return redirect()->route('brands.index')
+            ->with('succes', 'Brand created succesfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Brand $brand)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Brand $brand)
+    {
+        return view('BrandViews.brand-edit',compact('brand'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Brand $brand)
+    {
+        $brand->update([
+           'name' => $request -> input('name')
+        ]);
+        return redirect()->route('brands.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Brand $brand)
+    {
+        $brand->delete();
+        return redirect()->route('brands.index');
+    }
+}
