a399cd133d7f4e25e755e000a5a7665217a308a9
Axel Fontana <axelfontana09@gmail.com>
2023-04-30 20:41:29 -0300

    Name max length is now 30

--- database/migrations/2023_03_26_0002_create_products_table.php
+++ database/migrations/2023_03_26_0002_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name', 255);
+            $table->string('name', 30);
             $table->string('size', 20);
             $table->string('image');
             $table->unsignedInteger('price');
