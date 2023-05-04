cd995b617a3805fce74ee21fe8c32a5428158620
Axel Fontana <axelfontana09@gmail.com>
2023-05-04 19:41:43 -0300

    Added style to the edits forms

--- resources/views/BrandViews/brand-edit.blade.php
+++ resources/views/BrandViews/brand-edit.blade.php
@@ -1,13 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <form method="POST" action="{{ route('brands.update', $brand) }}">
-        @csrf
-        @method('PUT')
-        <div class="form-group">
-            <label for="name">Name:</label>
-            <input type="text" name="name" id="name" class="form-control" value="{{ $brand->name }}">
-        </div>
-        <button type="submit" class="btn btn-primary">Update Brand</button>
-    </form>
+    <div class="container">
+        <form method="POST" action="{{ route('brands.update', $brand) }}">
+            @csrf
+            @method('PUT')
+            <div class="form-group">
+                <label for="name">Name:</label>
+                <input type="text" name="name" id="name" class="form-control" value="{{ $brand->name }}">
+            </div>
+            <button type="submit" class="btn btn-primary mt-2">Update Brand</button>
+        </form>
+    </div>
 @endsection
--- resources/views/CategoryViews/category-edit.blade.php
+++ resources/views/CategoryViews/category-edit.blade.php
@@ -1,13 +1,16 @@
 @extends('layouts.edit-form')
 
 @section('content')
-<form method="POST" action="{{ route('categories.update', $category->id) }}">
-    @csrf
-    @method('PUT')
-    <div class="form-group">
-        <label for="name">Name:</label>
-        <input type="text" name="name" id="name" class="form-control" value="{{ $category->name }}">
+    <div class="container">
+        <h1>Edit Category</h1>
+        <form method="POST" action="{{ route('categories.update', $category->id) }}">
+            @csrf
+            @method('PUT')
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" name="name" id="name" class="form-control" value="{{ $category->name }}">
+            </div>
+            <button type="submit" class="btn btn-primary mt-2">Update Category</button>
+        </form>
     </div>
-    <button type="submit" class="btn btn-primary">Update Category</button>
-</form>
 @endsection
