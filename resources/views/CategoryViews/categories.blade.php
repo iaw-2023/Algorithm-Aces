bfbd387d3c1723601422c039a2dea4fe2b2667c2
Tomás Joaquín Diez <42483742+TomasDiez99@users.noreply.github.com>
2023-05-04 19:58:14 -0300

    Update categories.blade.php
    Removed card style, added header and create button

--- resources/views/CategoryViews/categories.blade.php
+++ resources/views/CategoryViews/categories.blade.php
@@ -2,48 +2,46 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Categories</div>
-                    <div class="card-body">
-                        <table class="table table-striped">
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Manage</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($categories as $category)
-                                    <tr>
-                                        <td>{{ $category->name }}</td>
-                                        <td>
-                                            <a href="{{ route('categories.edit', $category) }}"
-                                                class="btn btn-primary btn-sm">Edit</a>
-                                            @if ($category->enable)
-                                                <form action="{{ route('categories.disable', $category) }}" method="POST"
-                                                    onsubmit="return confirm('Are you sure you want to disable this category?')">
-                                                    @csrf
-                                                    @method('PUT')
-                                                    <button type="submit" class="btn btn-success">Enabled</button>
-                                                </form>
-                                            @else
-                                                <form action="{{ route('categories.enable', $category) }}" method="POST"
-                                                    onsubmit="return confirm('Are you sure you want to enable this category?')">
-                                                    @csrf
-                                                    @method('PUT')
-                                                    <button type="submit" class="btn btn-danger">Disabled</button>
-                                                </form>
-                                            @endif
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+        <div class="container">
+            <div class="d-flex justify-content-between align-items-center">
+                <h1 class="ml-2">Categories</h1>
+                <a href="{{ route('categories.create') }}" class="btn btn-primary mr-2">Create new category</a>
+            </div>
+            <div class="table-responsive">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Manage</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($categories as $category)
+                            <tr>
+                                <td>{{ $category->name }}</td>
+                                <td>
+                                    <a href="{{ route('categories.edit', $category) }}"
+                                        class="btn btn-primary btn-sm">Edit</a>
+                                    @if ($category->enable)
+                                        <form action="{{ route('categories.disable', $category) }}" method="POST"
+                                            onsubmit="return confirm('Are you sure you want to disable this category?')">
+                                            @csrf
+                                            @method('PUT')
+                                            <button type="submit" class="btn btn-success">Enabled</button>
+                                        </form>
+                                    @else
+                                        <form action="{{ route('categories.enable', $category) }}" method="POST"
+                                            onsubmit="return confirm('Are you sure you want to enable this category?')">
+                                            @csrf
+                                            @method('PUT')
+                                            <button type="submit" class="btn btn-danger">Disabled</button>
+                                        </form>
+                                    @endif
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
-    </div>
-@endsection
+    @endsection
