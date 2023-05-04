8cf6bc829312a911e8d4a102a1d1b4975788cfbe
Axel Fontana <axelfontana09@gmail.com>
2023-05-04 17:29:29 -0300

    product rules fix

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,14 +11,14 @@ class Product extends Model
     ];
 
     public static $rules = [
-        'name' => 'required|regex:/^[a-zA-Z0-9\s]{1,30}$/|unique:products',
+        'name' => 'required|regex:/^[a-zA-Z0-9\s ]{1,30}$/|unique:products',
         'image' => 'required|string',
         'size' => 'required|regex:/^[a-zA-Z0-9]{1,20}$/',
         'price' => 'required|integer|min:0|max:9999',
         'stock' => 'required|integer|min:1|max:9999',
         'brand_id' => 'required|exists:brands,id',
         'category_id' => 'required|exists:categories,id',
-        'enable' => 'required|boolean',
+        'enable' => 'boolean',
     ];
 
     public function brand()
