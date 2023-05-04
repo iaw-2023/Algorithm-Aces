d00bde7e4b1ae9c91f7754db4a6e8c6bb8dea37e
Axel Fontana <axelfontana09@gmail.com>
2023-05-04 17:11:44 -0300

    error will only be displayed if there is any

--- resources/views/layouts/create-form.blade.php
+++ resources/views/layouts/create-form.blade.php
@@ -3,11 +3,14 @@
 
 
 @section('error-section')
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 @endsection
+
