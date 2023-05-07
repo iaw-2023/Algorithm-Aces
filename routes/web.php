375409d83c08dc0f56da10f791f8f6a564376b4d
Axel Fontana <axelfontana09@gmail.com>
2023-05-07 20:01:14 -0300

    Eliminated register route

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
-Auth::routes();
-
-
+Auth::routes(['register' => false]);
 
 //CategoryView route
 Route::middleware(['auth'])->group(function () {
