616a83879007771a656b4619142208f934e4ae37
Tomás Joaquín Diez <42483742+TomasDiez99@users.noreply.github.com>
2023-05-11 15:26:01 -0300

    fix api-docs location

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -77,7 +77,7 @@ return [
             /*
              * Absolute path to location where parsed annotations will be stored
             */
-            'docs' => storage_path('api-docs'),
+            'docs' => base_path('resources/views/vendor/l5-swagger'),
 
             /*
              * Absolute path to directory where to export views
