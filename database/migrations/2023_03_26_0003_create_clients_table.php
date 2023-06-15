3836c8b81d3b31665bbec462658ef63ece188ce7
Tomás Joaquín Diez <42483742+TomasDiez99@users.noreply.github.com>
2023-06-15 16:13:28 -0300

    fixed client migration and seeding

--- database/migrations/2023_03_26_0003_create_clients_table.php
+++ database/migrations/2023_03_26_0003_create_clients_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
 
         Schema::create('clients', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('email', 30)->unique();
+            $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
