b95ba84ce862c5223d119218d4ac4c661cd9e71f
Tomas Diez <dieztomasjoaquin@gmail.com>
2026-07-22 16:39:09 -0300

    upload image fixed env var

--- public/js/uploadImage.js
+++ public/js/uploadImage.js
@@ -7,8 +7,9 @@ const fileInput = document.getElementById('image-file');
 const uploadButton = document.getElementById('upload-button');
 const preview = document.getElementById('image-preview');
 
-const supabaseUrl = 'https://vvivrtywfxzzjrnepzmm.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2aXZydHl3Znh6empybmVwem1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE3Mzk3MDUsImV4cCI6MTk5NzMxNTcwNX0.K1glzkpERU0zS0dGvDx-RMoceIEJ0OKZh8-eWs1V4bk';
+
+const supabaseUrl = import.meta?.env?.VITE_SUPABASE_URL || 'not-defined';
+const supabaseKey = import.meta?.env?.VITE_SUPABASE_PUBLISHABLE_KEY || 'not-defined';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 async function uploadImage() {
